1cf35326d2dcb2b5832a9f57c3d506897dc43cbf
den <kickrop@gmail.com>
2018-10-07 22:14:48 +0300

    open overlay menu fuction

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 //button
 	$('.top-nav_btn').on('click', function(e) {
   e.preventDefault;
+  if($(this).hasClass('top-nav_btn_active')){
+  	$('.top-nav-btn-overlay').animate({top:'-100%'},'slow');
+  	$('.top-nav_btn').toggleClass('top-nav_btn_active');
+  }
+  else {
   $(this).toggleClass('top-nav_btn_active');
+  $('.top-nav-btn-overlay').toggleClass('top-nav-btn-overlay-active');
+  $('.top-nav-btn-overlay').animate({top:'0'},'slow');
+ }
 });
+
+
+// $('.next-button').click(function() {
+//     if($(this).parent().hasClass('specific-step')){
+//          performSpecificStep();
+//     }
+//     else{
+//         // Do standard stuff
+//     }
+// });
